d82a96e6a256b80fcd909411be8f001665aac8e3
Felix <steinke.felix@yahoo.de>
2020-06-08 23:58:25 +0200

    added basic functions of Pruefung - need to find a solution for Ü

--- KL-Planner/Pruefung.h
+++ KL-Planner/Pruefung.h
@@ -6,9 +6,39 @@
 #define KL_PLANNER_PRUEFUNG_H
 
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Utility.h"
+
+using namespace std;
+
 class Pruefung {
 
-};
+public:
+    Pruefung();
+    friend std::ostream& operator<<(std::ostream &out, const Pruefung &anmeldung);
+    static vector<Pruefung> parse(string pathToFile);
 
+private:
+    Pruefung(string& studiengang, int verteilt, int pVersion, int pNummer, string& pName,
+             int pPruefer1, string& pruefer1, int pPruefer2, string& pruefer2,
+             float pDauer, string& pForm, int pSemester, bool angeboten);
+
+    string studiengang;
+    int verteilt; //maybe bool
+    int pVersion;
+    int pNummer;
+    string pName;
+    int pPruefer1;
+    string pruefer1;
+    int pPruefer2;
+    string pruefer2;
+    float pDauer;
+    string pForm;
+    int pSemester;
+    bool angeboten;
+
+};
 
 #endif //KL_PLANNER_PRUEFUNG_H
