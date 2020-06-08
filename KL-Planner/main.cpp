2e180b88bb58e4d38837fda2ba7287b069bf1145
Felix <steinke.felix@yahoo.de>
2020-06-08 20:59:11 +0200

    Added parsing the file, creating the object is missing.

--- KL-Planner/main.cpp
+++ KL-Planner/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Raum.h"
 
 int main() {
+    Raum input;
+    input.parseRaumliste("C:\\Users\\stein\\CLionProjects\\KL-Planner-Github\\input\\Raumliste.csv");
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
