431aceb5c1035c9a97b1a99587be0457122e4658
Johannes Immiger <johannes.imminger@gmx.de>
2020-06-09 10:21:30 +0200

    fix Raum einlesen

--- KL-Planner/main.cpp
+++ KL-Planner/main.cpp
@@ -3,8 +3,8 @@
 #include "Pruefung.h"
 
 int main() {
-    //Raum::parseRaumliste("../../input/Raumliste.csv");
+    Raum::parseRaumliste("../../input/Raumliste.csv");
     //Anmeldung::parse("../../input/Anmeldungen_WS2019_KL.csv");
-    Pruefung::parse("../../input/Angebotene_Pruefungen_KL.csv");
+    //Pruefung::parse("../../input/Angebotene_Pruefungen_KL.csv");
     return 0;
 }
