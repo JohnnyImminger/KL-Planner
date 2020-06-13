cee21048d0a8e3e807acf184df42e8336556c05d
Johannes Immiger <johannes.imminger@gmx.de>
2020-06-13 15:30:34 +0200

    add count of elapsed execution time

--- KL-Planner/main.cpp
+++ KL-Planner/main.cpp
@@ -4,8 +4,12 @@
 
 #include "ReadInput.h"
 #include "Algorithmus.h"
+#include <chrono>
 
 int main() {
+    using namespace chrono;
+    milliseconds start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+
     ReadInput data;
     data.init();
     Algorithmus alg = Algorithmus(data);
@@ -15,5 +19,8 @@ int main() {
     alg.printRaumplanliste("../../output/raumPlanListe.csv");
     alg.printProfpalnliste("../../output/profPlanListe.csv");
     alg.printStudentplanliste("../../output/studentPlanListe.csv");
+
+    milliseconds end = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+    cout << "Executed in " << (end-start).count()/1000 << 's' << endl;
     return 0;
 }
